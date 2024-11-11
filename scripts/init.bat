aeb0c3c9367710451940dcbc9bf0adeeeb6d5074
AquIce <timeo.perrinjaquet@gmail.com>
2024-11-11 11:36:40 +0100

    Added init script

--- scripts/init.bat
+++ scripts/init.bat
@@ -1,6 +1,9 @@
 @echo off
 
-premake5 --file=build.lua gmake2
-make
-copy .\bin\SDL2.dll .\build\bin\run\DEBUG\SDL2.dll
-.\build\bin\run\DEBUG\ADL.exe
+mkdir bin
+cd bin
+curl -L -o SDL2.zip https://github.com/libsdl-org/SDL/releases/download/release-2.30.9/SDL2-devel-2.30.9-mingw.tar.gz
+tar -xf SDL2.zip
+copy SDL2-2.30.9\x86_64-w64-mingw32\bin\SDL2.dll SDL2.dll
+del SDL2.zip
+rd /S /Q SDL2-2.30.9
